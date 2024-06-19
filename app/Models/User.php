834ef51e2c1f7848a4a015e6d27b8af916af845f
Angela <angelascc16@gmail.com>
2024-06-19 15:18:49 -0500

    fix button visible

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,6 +58,15 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(User::class, 'friends', 'to_id', 'from_id');
     }
+
+    public function isRelated(User $user)
+    {
+        if (auth()->user()->id === $user->id) {
+            return true;
+        }
+
+        return $this->from()->where('to_id', $user->id)->exists() || $this->to()->where('from_id', $user->id)->exists();
+    }
     
     //friends
     public function friendsFrom()
